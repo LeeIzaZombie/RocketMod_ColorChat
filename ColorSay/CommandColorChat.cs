309513f2cce9d3ac161fa7c7473215d5ffee654b
LeeIzaZombie <leeizazombie@outlook.com>
2015-05-20 21:19:27 +0100

    Fixed Array out of index error.
    Now ready for release!

--- ColorSay/CommandColorChat.cs
+++ ColorSay/CommandColorChat.cs
@@ -21,67 +21,66 @@ namespace ColorSay
 
         public void Execute(RocketPlayer caller, string[] command)
         {
-            string color = command[0].ToLower(), message = "";
-            int i = 0;
-            if (command.Length != 0)
-            {
-                foreach (string space in command)
-                {
-                    if (i != 0)
-                    {
-                        message += space + " ";
-                    }
-                    i++;
-                }
-                switch (color)
-                {
-                    case "black":
-                        RocketChat.Say(message, Color.black);
-                        break;
-                    case "blue":
-                        RocketChat.Say(message, Color.blue);
-                        break;
-                    case "clear":
-                        RocketChat.Say(message, Color.clear);
-                        break;
-                    case "cyan":
-                        RocketChat.Say(message, Color.cyan);
-                        break;
-                    case "gray":
-                        RocketChat.Say(message, Color.gray);
-                        break;
-                    case "green":
-                        RocketChat.Say(message, Color.green);
-                        break;
-                    case "grey":
-                        RocketChat.Say(message, Color.grey);
-                        break;
-                    case "magenta":
-                        RocketChat.Say(message, Color.magenta);
-                        break;
-                    case "red":
-                        RocketChat.Say(message, Color.red);
-                        break;
-                    case "white":
-                        RocketChat.Say(message, Color.white);
-                        break;
-                    case "yellow":
-                        RocketChat.Say(message, Color.yellow);
-                        break;
-                    case "debug":
-                        RocketChat.Say(message, Color.Lerp(Color.blue, Color.red, 255));
-                        break;
-                    default:
-                        RocketChat.Say(caller, "Error: Color: " + color + " is not available.", Color.red);
-                        break;
-                }
-            }
-            else
+            if (command.Length == 0)
             {
                 RocketChat.Say(caller, "/csay (color) (message) - List of colors:", Color.yellow);
                 RocketChat.Say(caller, "black, blue, clear, cyan, gray, green, grey, magenta, red, white, yellow, debug.", Color.yellow);
+                return;
+            }
+            string color = command[0].ToLower(), message = "";
+            int i = 0;
+            foreach (string space in command)
+            {
+                if (i != 0)
+                {
+                    message += space + " ";
+                }
+                i++;
+            }
+            switch (color)
+            {
+                case "black":
+                    RocketChat.Say(message, Color.black);
+                    break;
+                case "blue":
+                    RocketChat.Say(message, Color.blue);
+                    break;
+                case "clear":
+                    RocketChat.Say(message, Color.clear);
+                    break;
+                case "cyan":
+                    RocketChat.Say(message, Color.cyan);
+                    break;
+                case "gray":
+                    RocketChat.Say(message, Color.gray);
+                    break;
+                case "green":
+                    RocketChat.Say(message, Color.green);
+                    break;
+                case "grey":
+                    RocketChat.Say(message, Color.grey);
+                    break;
+                case "magenta":
+                    RocketChat.Say(message, Color.magenta);
+                    break;
+                case "red":
+                    RocketChat.Say(message, Color.red);
+                    break;
+                case "white":
+                    RocketChat.Say(message, Color.white);
+                    break;
+                case "yellow":
+                    RocketChat.Say(message, Color.yellow);
+                    break;
+                case "debug":
+                    RocketChat.Say(message, Color.Lerp(Color.blue, Color.red, 255));
+                    break;
+                default:
+                    RocketChat.Say(caller, "Error: Color: " + color + " is not available.", Color.red);
+                    break;
             }
         }
+    
 
         public string Help
         {
